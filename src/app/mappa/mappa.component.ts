6173bd512b079d96da7eff1ab272ddc3b8daab36
francescoConforti <francesco.conforti.04@gmail.com>
2021-03-21 14:49:27 +0100

    removed useless code

--- src/app/mappa/mappa.component.ts
+++ src/app/mappa/mappa.component.ts
@@ -19,9 +19,32 @@ export class MappaComponent implements OnInit {
   }
 
   loadMap() {
-    this.myMap = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'ArOu7EQ5tMWMGG0UsGGxRKBYx4XfoCLXsceSt6ZCkloQ3urZS6MevEBDjaKwuFv7',
+    var map = new Microsoft.Maps.Map('#myMap', {
+      credentials: 'ArOu7EQ5tMWMGG0UsGGxRKBYx4XfoCLXsceSt6ZCkloQ3urZS6MevEBDjaKwuFv7',
+      center: new Microsoft.Maps.Location(45.071222, 7.685090)
     });
+
+    var location1 = new Microsoft.Maps.Location(45.071222, 7.685090);
+    var location2 = new Microsoft.Maps.Location(45.062049, 7.697619);
+    //Create custom Pushpin
+    var pc = new Microsoft.Maps.Pushpin(location1, {
+      title: 'Piazza Castello',
+      subTitle: 'City Center',
+      text: '1',
+      color: 'red'
+    });
+    var csm = new Microsoft.Maps.Pushpin(location2, {
+      title: 'Chiesa di Santa Maria',
+      subTitle: 'City Center',
+      text: '2',
+      color: 'blue'
+    });
+
+    //Add the pushpin to the map
+    map.entities.push(pc);
+    map.entities.push(csm);
+
+    //Add your post map load code here.
   }
 
 
