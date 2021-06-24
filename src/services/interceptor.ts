29d9a47f5c09222184a3a33466c3a3ec42c7b6ee
francescoConforti <francesco.conforti.04@gmail.com>
2021-06-24 17:58:57 +0200

    removed debug code

--- src/services/interceptor.ts
+++ src/services/interceptor.ts
@@ -17,12 +17,6 @@ export class AuthInterceptor implements HttpInterceptor {
     if (token != null) {
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
     }
-    console.log("interceptor");
-    console.log(authReq);
     return next.handle(authReq);
   }
 }
-/*
-export const authInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
-];*/
